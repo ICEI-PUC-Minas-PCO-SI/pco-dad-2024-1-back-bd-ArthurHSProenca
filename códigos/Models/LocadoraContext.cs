312588ef1fd825d62d23f50f93c9d84490c29516
Arthur Proença <ahsproenca@sga.pucminas.br>
2024-06-15 13:44:41 -0300

    Add files via upload

---
+++ códigos/Models/LocadoraContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LocadoraVeiculos.Models
+{
+    public class LocadoraContext : DbContext
+    {
+        public DbSet<Cliente> Cliente { get; set; } = null!;
+        public DbSet<Veiculo> Veiculo { get; set; } = null!;
+        public DbSet<Reserva> Reserva { get; set; } = null!;
+
+        public LocadoraContext(DbContextOptions<LocadoraContext> options)
+            : base(options) { }
+    }
+}
---
+++ códigos/Models/Veiculo.cs
@@ -0,0 +1,13 @@
+﻿namespace LocadoraVeiculos.Models
+{
+    public class Veiculo
+    {
+        public int Id { get; set; }
+        public string? Modelo { get; set; }
+        public string? Marca { get; set; }
+        public int Ano { get; set; }
+        public string? Placa { get; set; }
+        public string? Disponibilidade { get; set; }
+        public ICollection<Reserva>? Reserva { get; set; }
+    }
+}
